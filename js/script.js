74e309c50ba1fffaa00da0fd373a774b8fe2558d
Sergey <mons198x@gmail.com>
2021-04-13 02:37:56 +0300

    second DOM homework

--- js/script.js
+++ js/script.js
@@ -14,6 +14,17 @@
 
 'use strict';
 
+const advDiv = document.querySelector('.promo__adv'),
+    ads = advDiv.querySelectorAll('img'),
+    promoDiv = document.querySelector('.promo__bg'),
+    promoGenre = promoDiv.querySelector('.promo__genre'),
+    watchedList = document.querySelector('.promo__interactive-list'),
+    deleteButtons = watchedList.querySelectorAll('.delete'),
+    addForm = document.querySelector('.add'),
+    inputMovie = addForm.querySelector('.adding__input'),
+    addButton = addForm.lastElementChild,
+    loveCheckBox = addForm.querySelector('[type="checkbox"]');
+
 const movieDB = {
     movies: [
         "Логан",
@@ -21,14 +32,15 @@ const movieDB = {
         "Ла-ла лэнд",
         "Одержимость",
         "Скотт Пилигрим против..."
-    ]
+    ],
+    updateMovies: function() {
+        watchedList.innerHTML = '';
+        this.movies.map((item) => item.toUpperCase()).sort().forEach((item, i) => watchedList.insertAdjacentHTML("beforeend", `
+            <li class="promo__interactive-item">${i+1}. ${item}
+                <div class="delete"></div>
+            </li>`));
+    }
 };
-
-const advDiv = document.querySelector('.promo__adv'),
-    ads = advDiv.querySelectorAll('img'),
-    promoDiv = document.querySelector('.promo__bg'),
-    promoGenre = promoDiv.querySelector('.promo__genre'),
-    watchedList = document.querySelector('.promo__interactive-list');    
 
 // удаление рекламы
 ads.forEach(item => item.remove());
@@ -40,11 +52,28 @@ promoGenre.textContent = 'драма';
 promoDiv.style.backgroundImage = 'url(img/bg.jpg)';
 
 // интерактивный список
-watchedList.innerHTML = '';
-movieDB.movies.sort().forEach((item, i) => watchedList.insertAdjacentHTML("beforeend", `
-    <li class="promo__interactive-item">${i+1}. ${item}
-        <div class="delete"></div>
-    </li>`));
+movieDB.updateMovies();
 
+// добавление фильмов в список
+addButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (inputMovie.value && inputMovie.value.length <= 21) {
+        movieDB.movies.push(inputMovie.value);
+    } else if (inputMovie.value.length >21) {
+        movieDB.movies.push(inputMovie.value.slice(0, 20) + '...');
+    }
+    if (loveCheckBox.checked) {
+        console.log('Добавляем любимый фильм');
+    }    
+    movieDB.updateMovies();
+    inputMovie.value = '';
+    loveCheckBox.checked = false;
+});
 
-
+// удаление фильмов
+watchedList.addEventListener('click', function(event) {
+    if (event.target.className === 'delete') {
+        movieDB.movies.splice((+event.target.parentElement.textContent.slice(0,1) - 1), 1);
+        movieDB.updateMovies();
+    }
+});
